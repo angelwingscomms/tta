0c47ea4febab757d2bc2c2a3d52604e73b556a6c
Gold Edem Hogan <edge37@outlook.com>
2025-03-11 14:27:37 +0100

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,18 @@
 <h1>Christians Should Meet to Reason</h1>
 
-<div class="text-gray-500 text-sm mb-4">Bible (Young's Literal Translation)</div>
+<div class="text-gray-500 text-xs mb-4">Bible (Young's Literal Translation)</div>
 
 <div class="mb-4">
 <p class="font-mono text-gray-700">Acts 19:9</p>
-<p class="text-justify leading-relaxed">And when certain were hardened and were disbelieving, speaking evil of the way before the multitude, having departed from them, he did separate the disciples, every day reasoning in the school of a certain Tyrannus.</p>
+<p class="text-justify text-gray-800">And when certain were hardened and were disbelieving, speaking evil of the way before the multitude, having departed from them, he did separate the disciples, every day reasoning in the school of a certain Tyrannus.</p>
 </div>
 
 <div class="mb-4">
 <p class="font-mono text-gray-700">Acts 20:7</p>
-<p class="text-justify leading-relaxed">And on the first of the week, the disciples having been gathered together to break bread, Paul was discoursing to them, about to depart on the morrow, he was also continuing the discourse till midnight.</p>
+<p class="text-justify text-gray-800">And on the first of the week, the disciples having been gathered together to break bread, Paul was discoursing to them, about to depart on the morrow, he was also continuing the discourse till midnight.</p>
 </div>
 
-<p>We should meet to reason and discuss the Bible, it's interpretations and applications, not depending on 'ordained preachers' (which don't exist in the Bible by the way) to dictate the interpretation of the scriptures to us</p>
+<p class="text-gray-800">We should meet to reason and discuss the Bible, its interpretations and applications, not depending on 'ordained preachers' (which don't exist in the Bible by the way) to dictate the interpretation of the scriptures to us.</p>
 
-<p class="text-lg leading-relaxed mb-4">Don't wait for some "preacher" to tell us what the Bible means.</p>
-<p class="text-lg leading-relaxed mb-4">The preacher isn't the only one blessed with knowledge.  (1 Corinthians 12:4-6: "Now there are diversities of gifts, but the same Spirit. And there are differences of administrations, but the same Lord. And there are diversities of operations, but it is the same God who worketh all in all.")</p>
+<p class="text-lg font-medium text-gray-800 mb-4">Don't wait for some "preacher" to tell us what the Bible means.</p>
+<p class="text-lg font-medium text-gray-800 mb-4">The preacher isn't the only one blessed with knowledge.  (1 Corinthians 12:4-6: "Now there are diversities of gifts, but the same Spirit. And there are differences of administrations, but the same Lord. And there are diversities of operations, but it is the same God who worketh all in all.")</p>
